fc2ef8d6435755e7d53728fdd56cbef5d33c7057
gousummer <gousummer@gmail.com>
2026-03-04 16:58:08 +0800

    Update DAY1_ClickCounter.sol

--- Summergou/DAY1_ClickCounter.sol
+++ Summergou/DAY1_ClickCounter.sol
@@ -2,14 +2,13 @@
 pragma solidity ^0.8.0;
 
 contract ClickCounter {
-    // 状态变量 - 存储点击次数
     uint256 public counter;
 
-    // 函数 - 增加计数器
     function click() public {
         counter++;
     }
 }
+
 
 
 
